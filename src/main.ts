8101dd7e320fe3b90456ac12a21f767c2a3ffdda
ShirBaruchian <shirush.bar@gmail.com>
2024-05-04 12:49:35 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('Private Chef API')
+  .setDescription('')
   .setVersion('1.0')
-  .addTag('cats')
+  .addTag('recipe')
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
